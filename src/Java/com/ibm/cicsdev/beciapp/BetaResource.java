3edd6a9ab4fefdf1db89dd7825514bba30205097
Wojciech Kłopotek <wojtek.klopotek@gmail.com>
2021-02-25 23:52:07 +0100

    skomentowana java 2

--- src/Java/com/ibm/cicsdev/beciapp/BetaResource.java
+++ src/Java/com/ibm/cicsdev/beciapp/BetaResource.java
@@ -1,13 +1,3 @@
-/* Licensed Materials - Property of IBM                                   */
-/*                                                                        */
-/* SAMPLE                                                                 */
-/*                                                                        */
-/* (c) Copyright IBM Corp. 2016 All Rights Reserved                       */
-/*                                                                        */
-/* US Government Users Restricted Rights - Use, duplication or disclosure */
-/* restricted by GSA ADP Schedule Contract with IBM Corp                  */
-/*                                                                        */
-
 package com.ibm.cicsdev.beciapp;
 
 import java.text.MessageFormat;
